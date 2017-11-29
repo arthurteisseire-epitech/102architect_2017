0c3cdf15c8e8c82c6201f5c706118b34772a8a3b
Mahé Caurette <Ozz@localhost.localdomain>
2017-11-29 17:32:29 +0100

    [+] coord_to_matrice [+] matrice_product

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3.6
+from matrice import *
+
+matrice_1 = [[2, -5, 1], [0, 3, 4], [-7, 1, 8]]
+matrice_2 = [[3], [-1], [2]]
+print(matrice_1)
+print(matrice_2)
+matrice_res = matrice_product(matrice_1, matrice_2)
+print(matrice_res)
