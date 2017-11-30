67e92fc6d1efcb0932961e8268e939f46abb5c74
Mahé Caurette <Ozz@localhost.localdomain>
2017-11-30 16:57:36 +0100

    [*] main.py -- import parse.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.6
 from matrix import *
-#from transformation import *
+from transformation import *
+from parse import *
 
 A = [[2, -5, 1], [0, 3, 4], [-7, 1, 8]]
 B = [[3, 2, 2], [-1, 3, 3], [2, 4, 4]]
