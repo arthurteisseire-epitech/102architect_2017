056804e7a7e038770a04ad8bb72f5e255d54140c
Arthur <arthur.teisseire@epitech.eu>
2017-12-01 12:24:53 +0100

    print_res now handle -0.0

--- print_res.py
+++ print_res.py
@@ -1,2 +1,6 @@
-def print_res(x, y, matrix):
-    print("(%d,%d) => (%.2f,%.2f)" %(x, y, matrix[0][0], matrix[1][0]))
+def print_res(x, y, xpos):
+    if (xpos[0][0] > -0.01 and xpos[0][0] < 0):
+        xpos[0][0] *= -1
+    if (xpos[1][0] > -0.01 and xpos[1][0] < 0):
+        xpos[1][0] *= -1
+    print("(%d,%d) => (%.2f,%.2f)" %(x, y, xpos[0][0], xpos[1][0]))
